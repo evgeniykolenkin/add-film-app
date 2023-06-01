61708dc591974bb79d54e871894ad1cce0ce7c37
Evgeny Kolenkin <immaim@yandex.ru>
2023-06-01 19:19:58 +0300

    huge refactoring and rebuilding of js code with another way of logic

--- script/main.js
+++ script/main.js
@@ -1,86 +1,114 @@
 // строковые константы
 const CHECKED_CLASS_NAME = "checked";
-const LIST_EL_CLASS_NAME = "add__app-film";
-const BTN_CHECK_CLASS_NAME = "btn__check-film";
-const BTN_DELETE_CLASS_NAME = "btn__delete-film";
 
 // константы из html
+const formNode = document.getElementById("add__app-form");
 const inputNode = document.getElementById("add__app-input");
 const addBtnNode = document.getElementById("btn__add-film");
 const listNode = document.getElementById("add__app-list");
-const btnCheck = document.querySelector(".btn__check-film");
+const emptyElement = document.getElementById("empty__item-list");
 
-// наша главная функция создания элемента списка
-function createDeleteFilms() {
-  // значения в инпуте будут равны названию фильма
-  const titleFilm = inputNode.value;
+// обработчики событий----------------------------------
 
-  // создаем элемент li
-  const li = document.createElement("li");
-  li.className = LIST_EL_CLASS_NAME;
-  li.innerHTML = "";
+// добавление фильма
+formNode.addEventListener("submit", addFilm);
 
-  // добавляем в список ul элемент li
-  listNode.appendChild(li);
-  // saveData();
+// удаление фильма
+listNode.addEventListener("click", deleteFilm);
 
-  // создаем кнопки
-  const btnCheck = document.createElement("button");
-  btnCheck.className = BTN_CHECK_CLASS_NAME;
-  btnCheck.innerHTML = `
-  <img src="resources/unchecked.png" alt="circle" />
-  <h2>${titleFilm}</h2>`;
+// отмечаем фильм просмотренным
+listNode.addEventListener("click", checkedFilm);
 
-  const btnDelete = document.createElement("button");
-  btnDelete.className = BTN_DELETE_CLASS_NAME;
-  btnDelete.innerHTML = `
-  <img src="resources/btn-cross.png" alt="cross" />`;
+// редактируем и сохраняем название
+listNode.addEventListener("click", editFilm);
+listNode.addEventListener("click", saveEdit);
 
-  // добавили в элемент li кнопки
-  li.appendChild(btnCheck);
-  li.appendChild(btnDelete);
+// функции----------------------------------------------
 
-  // вешаем обработчики событий на них
-  // переключаем класс checked
-  btnCheck.addEventListener("click", () => {
-    li.classList.toggle(CHECKED_CLASS_NAME);
-    // saveData();
-  });
+function addFilm(e) {
+  // отменяем отправку формы
+  e.preventDefault();
 
-  // удаляем один из элементов li
-  btnDelete.addEventListener("click", () => {
-    listNode.removeChild(li);
-    // saveData();
-  });
+  // достаем текст из инпута
+  const filmText = inputNode.value;
+
+  // формируем разметку для нового фильма
+  const filmHTML = `
+  <li class="add__app-film">
+    <div class="col__left">
+      <button data-action="checked" class="btn btn__check-film">
+        <img src="resources/unchecked.png" alt="" />
+      </button>
+      <input class="film__title" readonly value="${filmText}" />
+    </div>
+    <div class="col__right">
+      <button data-action="edit" class="btn btn__edit-film">
+        <img src="resources/edit.png" class="edit__img" alt="" />
+      </button>
+      <button data-action="save" class="btn btn__save-edit hidden">
+        <img
+          src="resources/favicon.ico"
+          class="save__img"
+          alt=""
+        />
+      </button>
+      <button data-action="delete" class="btn btn__delete-film">
+        <img src="resources/btn-cross.png" alt="" />
+      </button>
+    </div>
+  </li>`;
+
+  // добавляем ее на страницу
+  listNode.insertAdjacentHTML("beforeend", filmHTML);
+
+  // очищаем инпут и оставляем фокус на нем
+  inputNode.value = "";
+  inputNode.focus();
+
+  // убираем первый элемент списка с img пустого листа
+  // когда есть хотя бы один добавленный фильм
+  if (listNode.children.length > 1) {
+    emptyElement.classList.add("hidden");
+  }
 }
 
-// добавляем значение в инпуте inputNode через клик по кнопке addBtnNode
-// привязываем тоже самое на keyup
-addBtnNode.addEventListener("click", () => {
-  if (!inputNode.value) {
-    alert("Введите название фильма");
-    return;
+function deleteFilm(e) {
+  if (e.target.dataset.action === "delete") {
+    const parentNode = e.target.closest(".add__app-film");
+    parentNode.remove();
   }
-  createDeleteFilms(inputNode.value);
-  inputNode.value = "";
-  // saveData();
-});
-
-inputNode.addEventListener("keyup", function (e) {
-  if (e.code === "Enter") {
-    addBtnNode.click();
+  if (listNode.children.length === 1) {
+    emptyElement.classList.remove("hidden");
   }
-});
+}
 
-// сохранение в localStorage
-// не понимаю, почему я не могу изменять данные из localStorage
+function checkedFilm(e) {
+  if (e.target.dataset.action === "checked") {
+    const parentNode = e.target.closest(".add__app-film");
+    parentNode.classList.toggle("checked");
+  }
+}
 
-// function saveData() {
-//   localStorage.setItem("data", listNode.innerHTML);
-// }
+function editFilm(e) {
+  if (e.target.dataset.action === "edit") {
+    const parentNode = e.target.closest(".add__app-film");
+    const editBtnNode = parentNode.querySelector(".btn__edit-film");
+    const saveBtnNode = parentNode.querySelector(".btn__save-edit");
+    const filmTitle = parentNode.querySelector(".film__title");
+    saveBtnNode.classList.remove("hidden");
+    editBtnNode.classList.add("hidden");
+    filmTitle.removeAttribute("readonly");
+  }
+}
 
-// function getData() {
-//   listNode.innerHTML = localStorage.getItem("data");
-// }
-
-// getData();
+function saveEdit(e) {
+  if (e.target.dataset.action === "save") {
+    const parentNode = e.target.closest(".add__app-film");
+    const editBtnNode = parentNode.querySelector(".btn__edit-film");
+    const saveBtnNode = parentNode.querySelector(".btn__save-edit");
+    const filmTitle = parentNode.querySelector(".film__title");
+    saveBtnNode.classList.add("hidden");
+    editBtnNode.classList.remove("hidden");
+    filmTitle.setAttribute("readonly", true);
+  }
+}
